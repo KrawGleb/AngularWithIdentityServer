e0be99f4a7f77fe0bfdcc6ec401d2ecb773382ef
KrawGleb <krawcevitsch@gmail.com>
2022-07-19 22:25:45 +0300

    First IdentityServer profile

---
+++ AngularWithIdentityServer.IdentityServer/Configurations/InMemoryConfig.cs
@@ -0,0 +1,55 @@
+﻿using System.Security.Claims;
+using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace AngularWithIdentityServer.IdentityServer.Configurations;
+
+public static class InMemoryConfig
+{
+    public static IEnumerable<IdentityResource> GetIdentityResources() =>
+        new List<IdentityResource>
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+        };
+
+    public static List<TestUser> GetTestUsers() =>
+        new List<TestUser>
+        {
+            new TestUser
+            {
+                SubjectId = "C453D7B4-C493-47D9-A863-6677B3F8BB8B",
+                Username = "Mick",
+                Password = "MickPassword",
+                Claims = new List<Claim>
+                {
+                    new Claim("given_name", "Mick"),
+                    new Claim("family_name", "Mining"),
+                },
+            },
+            new TestUser
+            {
+                SubjectId = "BF7A8027-1181-4BA9-B6DE-1988141125B5",
+                Username = "Jane",
+                Password = "JanePassword",
+                Claims = new List<Claim>
+                {
+                    new Claim("given_name", "Jane"),
+                    new Claim("family_name", "Downing"),
+                },
+            },
+        };
+
+    public static IEnumerable<Client> GetClients() =>
+        new List<Client>
+        {
+            new Client
+            {
+                ClientId = "company-employee",
+                ClientSecrets = new [] { new Secret("secret".Sha512()) },
+                AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                AllowedScopes = { IdentityServerConstants.StandardScopes.OpenId }
+            }
+        };
+}
--- AngularWithIdentityServer.IdentityServer/Program.cs
+++ AngularWithIdentityServer.IdentityServer/Program.cs
@@ -1,6 +1,18 @@
+﻿using AngularWithIdentityServer.IdentityServer.Configurations;
+
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+
+
+// TODO: Change memory storage with db
+services.AddIdentityServer()
+    .AddInMemoryIdentityResources(InMemoryConfig.GetIdentityResources())
+    .AddTestUsers(InMemoryConfig.GetTestUsers())
+    .AddInMemoryClients(InMemoryConfig.GetClients())
+    .AddDeveloperSigningCredential();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseIdentityServer();
 
 app.Run();
